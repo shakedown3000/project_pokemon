016ae0fbc0741fb6c07c8a9a2d841e8a4e19922e
shakedown3000 <annalaube90@gmail.com>
2024-08-01 14:45:15 +0200

    Co-authored-by: Lisa Ly <lisayl1688@users.noreply.github.com>

--- src/components/SingleType/SingleType.tsx
+++ src/components/SingleType/SingleType.tsx
@@ -1,7 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { Result } from "../../Interfaces/IAllPokemons";
 import { IType } from "../../Interfaces/IType";
-import SinglePokemon from "../SinglePokemon/SinglePokemon";
 import { PokemonContext } from "../../context/PokemonContext";
 
 interface ISingleTypeProps {
@@ -10,26 +9,47 @@ interface ISingleTypeProps {
 
 const SingleType: React.FC<ISingleTypeProps> = (props) => {
   const [singleType, setSingleType] = useState<IType | null>(null);
-  // const pokemonContext = useContext(PokemonContext);
-  // if (pokemonContext?.allPokemon?.name === test) {
+  const pokemonContext = useContext(PokemonContext);
+  const [typeClicked, setTypeClicked] = useState<IType | null>(null);
+  const [valueChosen, setValueChosen] = useState<string | null>(null);
 
-  // }
+  console.log(typeClicked?.name);
 
+  // Buttons
   useEffect(() => {
-    fetch(`${props.item.url}`)
+    fetch(props.item.url)
       .then((res) => res.json())
       .then((data: IType) => setSingleType(data))
       .catch((err) => console.error("Error by fetching data", err));
   }, []);
 
-  const test = singleType?.pokemon.map((item, index) => {
-    console.log(item.pokemon.name);
-  });
+  function filterType(
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void {
+    event.preventDefault();
+    console.log("Button clicked");
+    const value = event.currentTarget.value;
+    console.log(value);
+    if (pokemonContext) {
+      fetch(value)
+        .then((res) => res.json())
+        .then((data: IType) => {
+          const typeArray = data.pokemon.map((element) => element.pokemon);
+          // Wir wollen die Daten in Result Array umwandeln, um PokeContext upzudaten, wir creates neues array
+          // Pokemon ist ein objekt array mit pokemon & Slot, und ich will da noch mal rein in pokemon (=> name und url. ist struktur von result)
+          pokemonContext.setAllPokemon(typeArray);
+          setValueChosen(data.name);
+        })
+        .catch((err) => console.error("Error by fetching data", err));
+    }
+  }
 
   return (
     <>
-      <h3>{props.item.name}</h3>
-      <p>{props.item.url}</p>
+      <h1>You chose: {valueChosen}</h1>
+      <button value={props.item.url} onClick={filterType}>
+        {props.item.name}
+      </button>
     </>
   );
 };
--- src/components/TypeCategorization/TypeCategorization.tsx
+++ src/components/TypeCategorization/TypeCategorization.tsx
@@ -1,44 +1,31 @@
 import { useEffect, useState } from "react";
 import "./TypeCategorization.css";
-import { Result } from "../../Interfaces/IAllPokemons";
-import { Link } from "react-router-dom";
+import { IallPokemons, Result } from "../../Interfaces/IAllPokemons";
 import SingleType from "../SingleType/SingleType";
 
-// Alle Pokemon von ein bestimmten Type:
-// https://pokeapi.co/api/v2/type/${idParams} (zb: 2)
-// Alle Typs: https://pokeapi.co/api/v2/type/
-
-interface ITypeCategorization {
-  count: number;
-  next: string;
-  previous: null;
-  results: Result[];
-}
+// interface ITypeCategorization {
+//   count: number;
+//   next: string;
+//   previous: null;
+//   results: Result[];
+// }
 
 const TypeCategorization = () => {
-  // const pokemonContext = useContext(PokemonContext);
-  // muss gleich sein mit data.results (also Results[])
   const [typePokemon, setTypePokemon] = useState<Result[] | null>(null);
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/type/")
       .then((res) => res.json())
-      .then((data: ITypeCategorization) => setTypePokemon(data.results))
+      .then((data: IallPokemons) => setTypePokemon(data.results))
       .catch((err) => console.error("Error by fetching data", err));
   }, []);
-
-  // Alle Pokemon von ein bestimmten Type:
-  // https://pokeapi.co/api/v2/type/${idParams} (zb: 2)
 
   return (
     <div>
       <h2>Types</h2>
       {/* <Link to={`/detailpage/${singlePokemon?.name}`}>!!! */}
       {typePokemon?.map((typeObject, index) => (
-        <SingleType key={index} item={typeObject}>
-          <p>{typeObject.name}</p>
-          <p></p>
-        </SingleType>
+        <SingleType key={index} item={typeObject}></SingleType>
       ))}
     </div>
   );
